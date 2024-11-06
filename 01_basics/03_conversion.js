24c4b936b06a2376b51e692cfe8dade85d97cc9b
AbinChakraborty1 <abin.chakraborty@aot.edu.in>
2024-11-06 12:01:04 +0000

    conversion file added

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,11 @@
+/*let str1 = "3"
+let str2 = "4"
+console.log(str1+str2);
+console.log(typeof(str1));
+
+let int = typeof(Number(str1))
+console.log(int);*/
+let bool = null
+
+console.log(typeof(bool));
+
