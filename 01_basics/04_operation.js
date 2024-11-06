275fb25e165132c2d5216619a3474661039f4987
AbinChakraborty1 <abin.chakraborty@aot.edu.in>
2024-11-06 12:17:36 +0000

    ops

--- 01_basics/04_operation.js
+++ 01_basics/04_operation.js
@@ -1,7 +1,9 @@
 /*console.log("1"+1);
 console.log("1"+1+2);
 console.log(1+1+"2");
-console.log(5+1+"1"+"2");*/
+console.log(5+1+"1"+"2");
 
 console.log(+true);
-console.log(true);
+console.log(true);*/
+
+console.log("");
